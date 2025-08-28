b4fe4baa0903263c910979d2cbc01cfdcb8cb972
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2025-08-27 21:38:17 -0300

    update script

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <sstream>
+
 
 void printLine(const std::string & text) {
     std::cout << text << std::endl;
@@ -30,12 +32,63 @@ void clearTerminal() {
 }
 
 
+int calculator(int number_a, char number_operator, int number_b) {
+    int result = 0;
+    switch (number_operator) {
+        default:;
+        case '+': result = number_a + number_b; break;
+        case '-': result = number_a - number_b; break;
+        case '*': result = number_a * number_b; break;
+        case '/':
+            if (number_b != 0) result = number_a / number_b;
+            else std::cout << "ERROR: Division by zero\n";
+            break;
+    }
+    return result;
+}
+
+
+double parseText(std::string & text) {
+    std::istringstream iss(text);
+    long long number_a, number_b;
+    char op;
+
+    if (!(iss >> number_a >> op >> number_b)) {
+        std::cout << "ERROR: Invalid input" << std::endl;
+        return 1;
+    }
+
+    long long result;
+    switch (op) {
+        case '+': result = number_a + number_b; break;
+        case '-': result = number_a + number_b; break;
+        case '*': result = number_a + number_b; break;
+        case '/':
+            if (number_b != 0) result = number_a / number_b;
+            else {
+                std::cout << "ERROR: Division by zero\n";
+                return 1;
+            }
+            break;
+        default:
+            std::cout << "ERROR: Unknown operator" << std::endl;
+            return 1;
+    }
+
+    std::cout << "Result: " << result << std::endl;
+    return result;
+}
+
+
 int main() {
+    clearTerminal();
     for (int i = 0; i < 10; i++) {
         clearTerminal();
         std::string input = getLine("Select a option:\n [1] calculator\nR: ");
         if (input == "1") {
-            printLine("Access granted");
+            clearTerminal();
+            std::string to_calcule = getLine("Insert the calcule: ");
+            parseText(to_calcule);
         }
         else {
             printLine("Access denied");
